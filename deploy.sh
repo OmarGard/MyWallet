5eed1ca68274538aca3047a485f34e5cd0f5e89e
omargard <44001951+OmarGard@users.noreply.github.com>
2020-04-12 17:48:24 -0500

    modify deploy_sh

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,8 @@
-php artisan key:generate
 php artisan cache:clear
 php artisan route:clear
 php artisan config:clear 
 php artisan view:clear 
 # Run database migrations
-php artisan migrate
+php artisan migrate:fresh
 # Run database seeds
 php artisan db:seed
